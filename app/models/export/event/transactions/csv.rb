3b38db7b43cc8184362df4d38dfd020acf2e129d
Anish <22670462+anishanne@users.noreply.github.com>
2026-04-30 09:02:30 -0700

    add amount dollars to export (#13592)
    ## Summary of the problem
    Requested by team at FIRST worlds
    
    
    ## Describe your changes
    Add additional column to export

--- app/models/export/event/transactions/csv.rb
+++ app/models/export/event/transactions/csv.rb
@@ -70,12 +70,14 @@ class Export
         end
 
         def row(ct)
+          amount_cents = public_only && ct.likely_account_verification_related? ? 0 : ct.amount_cents
           SafeCsv::Row.new(
             headers,
             [
               ct.date,
               ct.local_hcb_code.memo,
-              public_only && ct.likely_account_verification_related? ? 0 : ct.amount_cents,
+              amount_cents,
+              format("%.2f", amount_cents / 100.0),
               ct.local_hcb_code.tags.filter { |tag| tag.event_id == event_id }.pluck(:label).join(", "),
               public_only ? "" : ct.local_hcb_code.comments.not_admin_only.pluck(:content).join("\n\n"),
               ct.local_hcb_code.author&.public_id || "",
@@ -87,7 +89,7 @@ class Export
         end
 
         def headers
-          [:date, :memo, :amount_cents, :tags, :comments, :user_id, :user_name, :category_slug, :category_label]
+          [:date, :memo, :amount_cents, :amount, :tags, :comments, :user_id, :user_name, :category_slug, :category_label]
         end
 
       end
